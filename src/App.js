13f39084f5baf5a6c26665c21e470b5bce6f3917
vadim-feof <plyto12@gmail.com>
2022-02-05 17:50:18 +0300

    added navlink and class activeLink for NavLink

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import Profile from './components/Profile/Profile';
 import Sidebar from './components/Sidebar/Sidebar';
 import Dialogs from "./components/Dialogs/Dialogs";
 import {BrowserRouter, Route, Routes} from "react-router-dom";
+import Settings from "./components/Settings/Settings";
+import News from "./components/News/News";
+import Music from "./components/Music/Music";
 
 /* http://preview.themeforest.net/item/buddy-multipurpose-wordpressbuddypress-theme/full_screen_preview/3506362?_ga=2.214380447.908696352.1643398348-404296411.1634847135
 */
@@ -17,6 +20,9 @@ function App() {
                     <Routes>
                         <Route path='/profile' element={<Profile />} />
                         <Route path='/dialogs' element={<Dialogs />} />
+                        <Route path='/news' element={<News />} />
+                        <Route path='/music' element={<Music />} />
+                        <Route path='/settings' element={<Settings />} />
                     </Routes>
                 </div>
             </div>
--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,23 +1,31 @@
 import React from 'react';
 import classes from './Sidebar.module.css';
+import {NavLink} from "react-router-dom";
+
+
 
 const Sidebar = () => {
+    // для обработки активной ссылки
+    const activeLink = () => {
+        return navData => navData.isActive ? classes.activeLink : classes.item
+    }
+
     return (
         <nav className={classes.sidebar}>
             <div className={classes.item}>
-                <a href='/profile'>Profile</a>
+                <NavLink to='/profile' className={activeLink()}>Profile</NavLink>
             </div>
             <div className={classes.item}>
-                <a href='/dialogs'>Messages</a>
+                <NavLink to='/dialogs' className={activeLink()}>Messages</NavLink>
             </div>
             <div className={classes.item}>
-                <a href='#'>News</a>
+                <NavLink to='/news' className={activeLink()}>News</NavLink>
             </div>
             <div className={classes.item}>
-                <a href='#'>Music</a>
+                <NavLink to='/music' className={activeLink()}>Music</NavLink>
             </div>
             <div className={classes.item}>
-                <a href='#'>Settings</a>
+                <NavLink to='/settings' className={activeLink()}>Settings</NavLink>
             </div>
         </nav>
     );

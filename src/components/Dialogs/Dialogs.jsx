675e1d8acd61f4ff921f34d3d7961428f9ed7b1b
vadim-feof <plyto12@gmail.com>
2022-02-07 23:00:26 +0300

    21-22-create dialogs item and message item

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,10 +1,39 @@
 import React from 'react';
 import classes from './Dialogs.module.css';
+import {NavLink} from "react-router-dom";
 
 const Dialogs = () => {
+    // для обработки активной ссылки
+    const activeDialogsItem = () => {
+        return navData => navData.isActive ? classes.activeDialogsItem : classes.dialogsItem
+    }
     return (
-        <div>
-            Dialogs
+        <div className={classes.wrapper}>
+            <div className={classes.dialogs}>
+                <NavLink to='/dialogs/1' className={activeDialogsItem()}>
+                    Angelina
+                </NavLink>
+                <NavLink to='/dialogs/2' className={activeDialogsItem()}>
+                    Anton
+                </NavLink>
+                <NavLink to='/dialogs/3' className={activeDialogsItem()}>
+                    Igor
+                </NavLink>
+                <NavLink to='/dialogs/4' className={activeDialogsItem()}>
+                    Sashka
+                </NavLink>
+            </div>
+            <div className={classes.messages}>
+                <div className={classes.messagesItem}>
+                    Hi!
+                </div>
+                <div className={classes.messagesItem}>
+                    How are you?
+                </div>
+                <div className={classes.messagesItem}>
+                    Hey
+                </div>
+            </div>
         </div>
     );
 }

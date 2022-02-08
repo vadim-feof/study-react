f9432f5722521a58629d0411f3bb5200206d61b4
vadim-feof <plyto12@gmail.com>
2022-02-08 17:19:00 +0300

    25-added .map for Posts and Dialogs

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -5,31 +5,29 @@ import MessagesItem from "./MessagesItem/MessagesItem";
 
 const Dialogs = () => {
 
-    let DialogsData = [
+    let dialogsData = [
         {id: 1, name: 'Angelina'},
         {id: 2, name: 'Anton'},
         {id: 3, name: 'Igor'},
         {id: 4, name: 'Sashka'}
     ]
 
-    let MessagesData = [
+    let messagesData = [
         {id: 1, message: 'Hi!'},
         {id: 2, message: 'How are you?'},
         {id: 3, message: 'Hey'}
     ]
 
+    let dialogsElement = dialogsData.map( d => <DialogsItem id={d.id} name={d.name}/>)
+    let messagesElement = messagesData.map( m => <MessagesItem id={m.id} message={m.message}/>)
+
     return (
         <div className={classes.wrapper}>
             <div className={classes.dialogs}>
-                <DialogsItem id={DialogsData[0].id} name={DialogsData[0].name}/>
-                <DialogsItem id={DialogsData[1].id} name={DialogsData[1].name}/>
-                <DialogsItem id={DialogsData[2].id} name={DialogsData[2].name}/>
-                <DialogsItem id={DialogsData[3].id} name={DialogsData[3].name}/>
+                { dialogsElement }
             </div>
             <div className={classes.messages}>
-                <MessagesItem message={MessagesData[0].message}/>
-                <MessagesItem message={MessagesData[1].message}/>
-                <MessagesItem message={MessagesData[2].message}/>
+                { messagesElement }
             </div>
         </div>
     );
--- src/components/Profile/Posts/Posts.jsx
+++ src/components/Profile/Posts/Posts.jsx
@@ -11,6 +11,8 @@ const Posts = () => {
         {id: 4, message: "Lorem ipsum dolor sit amet, consectetur adipisicing elit.", likeCount: 5},
         {id: 5, message: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium dicta in similique?", likeCount: 7}
     ]
+    
+    let postsItem = postsData.map( p => <PostItem id={p.id} likeCount={p.likeCount} message={p.message}/>)
 
     return (
         <div className={classes.posts}>
@@ -22,11 +24,7 @@ const Posts = () => {
                 <button>Create post</button>
             </div>
             <div>
-                <PostItem id={postsData[0].id} likeCount={postsData[0].likeCount} message={postsData[0].message}/>
-                <PostItem id={postsData[1].id} likeCount={postsData[1].likeCount} message={postsData[1].message}/>
-                <PostItem id={postsData[2].id} likeCount={postsData[2].likeCount} message={postsData[2].message}/>
-                <PostItem id={postsData[3].id} likeCount={postsData[3].likeCount} message={postsData[3].message}/>
-                <PostItem id={postsData[4].id} likeCount={postsData[4].likeCount} message={postsData[4].message}/>
+                { postsItem }
             </div>
         </div>
     );

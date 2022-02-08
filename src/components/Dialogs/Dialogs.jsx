4a1aa1f4e7c3515fca29e850eb032f00341ac746
vadim-feof <plyto12@gmail.com>
2022-02-08 16:38:08 +0300

    24-UI-BLL create objects data for Dialogs and Posts

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -4,18 +4,32 @@ import DialogsItem from "./DialogsItem/DialogsItem";
 import MessagesItem from "./MessagesItem/MessagesItem";
 
 const Dialogs = () => {
+
+    let DialogsData = [
+        {id: 1, name: 'Angelina'},
+        {id: 2, name: 'Anton'},
+        {id: 3, name: 'Igor'},
+        {id: 4, name: 'Sashka'}
+    ]
+
+    let MessagesData = [
+        {id: 1, message: 'Hi!'},
+        {id: 2, message: 'How are you?'},
+        {id: 3, message: 'Hey'}
+    ]
+
     return (
         <div className={classes.wrapper}>
             <div className={classes.dialogs}>
-                <DialogsItem id={'1'} name={'Angelina'}/>
-                <DialogsItem id={'2'} name={'Anton'}/>
-                <DialogsItem id={'3'} name={'Igor'}/>
-                <DialogsItem id={'4'} name={'Sashka'}/>
+                <DialogsItem id={DialogsData[0].id} name={DialogsData[0].name}/>
+                <DialogsItem id={DialogsData[1].id} name={DialogsData[1].name}/>
+                <DialogsItem id={DialogsData[2].id} name={DialogsData[2].name}/>
+                <DialogsItem id={DialogsData[3].id} name={DialogsData[3].name}/>
             </div>
             <div className={classes.messages}>
-                <MessagesItem message={'Hi!'}/>
-                <MessagesItem message={'How are you?'}/>
-                <MessagesItem message={'Hey'}/>
+                <MessagesItem message={MessagesData[0].message}/>
+                <MessagesItem message={MessagesData[1].message}/>
+                <MessagesItem message={MessagesData[2].message}/>
             </div>
         </div>
     );
--- src/components/Profile/Posts/Posts.jsx
+++ src/components/Profile/Posts/Posts.jsx
@@ -3,17 +3,30 @@ import classes from './Posts.module.css'
 import PostItem from "./PostItem/PostItem";
 
 const Posts = () => {
+
+    let postsData = [
+        {id: 1, message: "My first post", likeCount: 12},
+        {id: 2, message: "My second post", likeCount: 23},
+        {id: 3, message: "Lorem ipsum dolor sit amet.", likeCount: 11},
+        {id: 4, message: "Lorem ipsum dolor sit amet, consectetur adipisicing elit.", likeCount: 5},
+        {id: 5, message: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium dicta in similique?", likeCount: 7}
+    ]
+
     return (
         <div className={classes.posts}>
             My posts
-            <textarea></textarea>
-            <button>Create post</button>
             <div>
-                <PostItem id={1} likeCount={12} message={'My first post'}/>
-                <PostItem id={2} likeCount={23} message={'My second post'}/>
-                <PostItem id={3} likeCount={11} message={'Lorem ipsum dolor sit amet.'}/>
-                <PostItem id={4} likeCount={5} message={'Lorem ipsum dolor sit amet, consectetur adipisicing elit.'}/>
-                <PostItem id={5} likeCount={7} message={'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium dicta in similique?'}/>
+                <textarea></textarea>
+            </div>
+            <div>
+                <button>Create post</button>
+            </div>
+            <div>
+                <PostItem id={postsData[0].id} likeCount={postsData[0].likeCount} message={postsData[0].message}/>
+                <PostItem id={postsData[1].id} likeCount={postsData[1].likeCount} message={postsData[1].message}/>
+                <PostItem id={postsData[2].id} likeCount={postsData[2].likeCount} message={postsData[2].message}/>
+                <PostItem id={postsData[3].id} likeCount={postsData[3].likeCount} message={postsData[3].message}/>
+                <PostItem id={postsData[4].id} likeCount={postsData[4].likeCount} message={postsData[4].message}/>
             </div>
         </div>
     );

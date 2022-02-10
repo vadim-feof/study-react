2c847c0f91ef62482c9d7706b1f6fe75420c5bec
vadim-feof <plyto12@gmail.com>
2022-02-10 16:43:58 +0300

    added input and send button into Dialogs

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -7,13 +7,23 @@ const Dialogs = (props) => {
     let dialogsElement = props.state.dialogsData.map(d => <DialogsItem id={d.id} name={d.name} avatarUrl={d.avatarUrl}/>)
     let messagesElement = props.state.messagesData.map(m => <MessagesItem id={m.id} message={m.message}/>)
 
+    let inputElement = React.createRef();
+    let sendMessage = () => {
+        alert(inputElement.current.value)
+    }
     return (
         <div className={classes.wrapper}>
-            <div className={classes.dialogs}>
+            <div className={classes.dialogs__list}>
                 {dialogsElement}
             </div>
-            <div className={classes.messages}>
-                {messagesElement}
+            <div className={classes.messages__wrapper}>
+                <div className={classes.messages__list}>
+                    {messagesElement}
+                </div>
+                <div className={classes.messages__input}>
+                    <textarea ref={inputElement}></textarea>
+                    <button onClick={sendMessage} className={classes.sendButton}>Send</button>
+                </div>
             </div>
         </div>
     );
--- src/redux/state.js
+++ src/redux/state.js
@@ -2,8 +2,8 @@ let state = {
     dialogsPage: {
         dialogsData: [
             {id: 1, name: 'Angelina', avatarUrl: 'https://sun9-66.userapi.com/impg/Z-T1iiIc0VFoncYEDqQPRJtg1COa07wrPgD4ww/iJrY_XuJCak.jpg?size=723x1080&quality=96&sign=d36f1845cd8639490e4deb191dd1776d&type=album'},
-            {id: 2, name: 'Anton', avatarUrl: 'https://sun9-5.userapi.com/impf/c858028/v858028787/be9a6/5BOAj0hn96Y.jpg?size=1005x1080&quality=96&sign=4708d19e493404cb01b71c567f2b32d0&type=album'},
-            {id: 3, name: 'Igor', avatarUrl: 'https://sun9-16.userapi.com/impg/rlx83L7JZ-oK5O3zbEMahD6Dt9eKq39v8xSufA/vCVAALyAvu0.jpg?size=1139x1593&quality=96&sign=144f44d66c3f24732514ec3032abe8df&type=album'},
+            {id: 2, name: 'Igor', avatarUrl: 'https://sun9-5.userapi.com/impf/c858028/v858028787/be9a6/5BOAj0hn96Y.jpg?size=1005x1080&quality=96&sign=4708d19e493404cb01b71c567f2b32d0&type=album'},
+            {id: 3, name: 'Anton', avatarUrl: 'https://sun9-16.userapi.com/impg/rlx83L7JZ-oK5O3zbEMahD6Dt9eKq39v8xSufA/vCVAALyAvu0.jpg?size=1139x1593&quality=96&sign=144f44d66c3f24732514ec3032abe8df&type=album'},
             {id: 4, name: 'Sashka', avatarUrl: 'https://sun9-44.userapi.com/impg/_sPJYuthRgfDFAJztk6TooHISZCYMdCczGPEeg/2bhiVPVSUjM.jpg?size=1620x2160&quality=96&sign=62e7d3ff3fe12ef6a6a0419f527ea3fe&type=album'}
         ],
         messagesData: [
